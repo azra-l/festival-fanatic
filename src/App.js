2a93dd89de1843781024baf97fce21c55a0e1929
Azra Lalji <azralalji@dhcp-128-189-29-40.ubcsecure.wireless.ubc.ca>
2022-06-07 12:29:14 -0700

    add festival card component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import './App.css';
+import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <div className="App">
-      <div className='description-container'>
+      <Navbar />
+      <div className="description-container">
         <p>Find festivals based on your spotify listening with one click</p>
-        <button className='get-started-btn'>Get started</button>
+        <button className="get-started-btn">Log in with Spotify</button>
       </div>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { BrowserRouter } from "react-router-dom";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Results from "./routes/results/results";
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="results" element={<Results />} />
+    </Routes>
+  </BrowserRouter>
 );
-
