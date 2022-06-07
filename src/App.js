75f033a6ba60f347182c248352c5337ae09df44c
Azra Lalji <azralalji@Azras-MacBook-Air.local>
2022-06-06 20:43:05 -0700

    add homepage styling

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='description-container'>
+        <p>Find festivals based on your spotify listening with one click</p>
+        <button className='get-started-btn'>Get started</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
