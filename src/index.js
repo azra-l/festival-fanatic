111ef00d1076dd0eac115d9f6ada319b83d1a750
Azra Lalji <azralalji@Azras-MacBook-Air.local>
2022-06-10 10:41:16 -0700

    add login page

--- src/index.js
+++ src/index.js
@@ -5,12 +5,14 @@ import App from "./App";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Results from "./routes/results/results";
 import TopArtists from "./routes/top-artists/top-artists";
+import LogIn from "./routes/login/login";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />} />
+      <Route path="/login" element={<LogIn/>}/>
       <Route path="results" element={<Results />} />
       <Route path="top-artists" element={<TopArtists />} />
     </Routes>
