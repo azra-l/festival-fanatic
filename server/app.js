299066a4b5ce96b26d986dfed07b25ad71526a12
Stanford Lin <stanfordlin@outlook.com>
2022-06-24 02:55:13 +0000

    Get events from spotify data

--- server/app.js
+++ server/app.js
@@ -1,12 +1,26 @@
+require('dotenv').config()
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var livereload = require("livereload");
+var connectLiveReload = require("connect-livereload");
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var bandsInTownRouter = require('./routes/bandsintown');
+
+const liveReloadServer = livereload.createServer();
+liveReloadServer.server.once("connection", () => {
+  setTimeout(() => {
+    liveReloadServer.refresh("/");
+  }, 100);
+});
+
 
 var app = express();
+
+app.use(connectLiveReload());
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -16,5 +30,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/bandsintown', bandsInTownRouter);
 
 module.exports = app;
