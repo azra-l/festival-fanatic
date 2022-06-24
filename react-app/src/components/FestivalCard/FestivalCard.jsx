2d19876af3b060e58b85cbe933f0bbb01d9552a2
Tristan Martinuson <tmartinuson@gmail.com>
2022-06-23 23:47:38 -0700

    removed unnecessary field

--- react-app/src/components/FestivalCard/FestivalCard.jsx
+++ react-app/src/components/FestivalCard/FestivalCard.jsx
@@ -8,7 +8,7 @@ import {deleteFestival, saveFestival} from "../../redux/festivals/reducer";
 export default function FestivalCard({ festival }) {
   const dispatch = useDispatch();
 
-  const { date, name, city, state, img, userDeleted, saved, id } = festival;
+  const { date, name, city, state, img, saved, id } = festival;
   const dateParsed = new Date(date);
 
   // from: https://stackoverflow.com/questions/12246394/how-to-get-month-from-string-in-javascript
