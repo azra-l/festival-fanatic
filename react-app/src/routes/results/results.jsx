d9631d3d3d72698cf0bdc9c4c94158a53adfcd41
Stanford Lin <stanfordlin@outlook.com>
2022-06-24 11:19:25 +0000

    Added temp results for festivals on redux

--- react-app/src/routes/results/results.jsx
+++ react-app/src/routes/results/results.jsx
@@ -1,9 +1,20 @@
 import Navbar from "../../components/Navbar/Navbar";
 import FestivalCard from "../../components/FestivalCard/FestivalCard";
 import "./results.css";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from 'react-redux';
+import { getUpcomingArtistEventsAsync } from "../../redux/festivals/thunks";
+import React, { useEffect } from 'react';
+
 
 export default function Results() {
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getUpcomingArtistEventsAsync("Illenium"));
+  }, []);
+
+
   const dummyFestivals = useSelector(
     (state) => state.festivals.festivals
   );
@@ -12,13 +23,18 @@ export default function Results() {
       <Navbar />
       <h2>Your results</h2>
 
-      {dummyFestivals ? (
+
+{/* TODO: Update dummy festivals */}
+      {JSON.stringify(dummyFestivals)}
+
+
+      {/* {dummyFestivals ? (
         dummyFestivals.map((festival) => (
           <FestivalCard festival={festival} key={festival.id} />
         ))
       ) : (
         <p>Loading or Error, No data {JSON.stringify(dummyFestivals)}</p>
-      )}
+      )} */}
     </>
   );
 }
