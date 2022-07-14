a36e2d8281918c990b0745ccb02d33295d540b25
Azra Lalji <azralalji1@gmail.com>
2022-07-14 03:19:46 +0000

    first attempt

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+const User = require('../models/User');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', async function(req, res, next) {
+  const result = await User.find();
+  res.send(result);
 });
+
+router.delete('/', async function(req, res, next) {
+  await User.collection.drop();
+  res.status(204).send();
+});
+
 
 module.exports = router;
