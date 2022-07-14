df6460ccfe941101dd603a7191a6104da8e7d750
Azra Lalji <azralalji1@gmail.com>
2022-07-14 21:20:13 +0000

    add patch endpoint

--- react-app/src/redux/festivals/service.js
+++ react-app/src/redux/festivals/service.js
@@ -1,20 +1,17 @@
 import { fest } from "./sampleResults";
 
-const getUpcomingArtistEvents = async (artistName) => {
+const getUpcomingArtistEvents = async (id) => {
   // TODO: replace this hardcoded ID once we figure out how to get this on the frontend
-  const response = await fetch(
-    `http://localhost:3001/users/531629552196d9cfd8b3cbdd85a1807b`,
-    {
-      // TODO: Investigate best way to store JWT token for security
-      // https://stackoverflow.com/questions/27067251/where-to-store-jwt-in-browser-how-to-protect-against-csrf?rq=1
-      method: "GET",
-      // Need credentials to pass cookie data into request
-      credentials: "include",
-      headers: {
-        "Access-Control-Allow-Origin": "http://localhost:3000",
-      },
-    }
-  );
+  const response = await fetch(`http://localhost:3001/users/${id}`, {
+    // TODO: Investigate best way to store JWT token for security
+    // https://stackoverflow.com/questions/27067251/where-to-store-jwt-in-browser-how-to-protect-against-csrf?rq=1
+    method: "GET",
+    // Need credentials to pass cookie data into request
+    credentials: "include",
+    headers: {
+      "Access-Control-Allow-Origin": "http://localhost:3000",
+    },
+  });
 
   const data = await response.json();
   if (!response.ok) {
@@ -26,8 +23,30 @@ const getUpcomingArtistEvents = async (artistName) => {
   return festivals;
 };
 
+const updateFestival = async ({ user, festival }) => {
+  const response = await fetch(`http://localhost:3001/users/${user}`, {
+    method: "PATCH",
+    body: JSON.stringify(festival),
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "http://localhost:3000",
+    },
+   
+  });
+
+  const data = await response.json();
+
+  if (!response.ok) {
+    const errorMsg = data?.message;
+    throw new Error(errorMsg);
+  }
+
+  return data;
+};
+
 const festivalService = {
   getUpcomingArtistEvents,
+  updateFestival,
 };
 
 export default festivalService;
