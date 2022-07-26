d9f98fc606bb331aec98d2bbe7ccfba3aaf4f3c7
Amman Zaman <amman.z.zaman@gmail.com>
2022-07-26 18:55:24 +0000

    added get endpoint that returns matching festivals

--- server/routes/festivals.js
+++ server/routes/festivals.js
@@ -1,13 +1,18 @@
 var express = require("express");
+const Festival = require("../models/Festival");
 var router = express.Router();
 const User = require("../models/User");
 
+/*  TODO need to include computed value of the length of intersection between artists and selectedArtists
+    This will allow sorting to happen on the database side, and also allow pagination for query optimization
+*/
 /* GET festivals that star artists in user's selectedArtist lineup*/
-router.get("/", async function (req, res, next) {
-const userIdFromSession = req.session.user.userId
-  const user = await User.findOne({ userId: userIdFromSession });
+router.get("/matched", async function (req, res, next) {
+  const userId = req.session.user.userId
+  const user = await User.findOne({ userId: userId });
   const selectedArtists = user.selectedArtists;
-  res.send(selectedArtists);
+  const matchingFestivals = await Festival.find({ artists: { $in: selectedArtists }});
+  res.send(matchingFestivals);
 });
 
 module.exports = router;
