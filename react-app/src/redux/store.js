1f87e7b030fd633cc7d69d7a68377d7b6d08ceb1
Amman Zaman <amman.z.zaman@gmail.com>
2022-07-26 19:35:50 +0000

    removed redundant spotifyReducer and sampleResults (since mock data is now in the backend)

--- react-app/src/redux/store.js
+++ react-app/src/redux/store.js
@@ -1,11 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
-import spotifyReducer from './spotify/reducer';
 import festivalsReducer from './festivals/reducer';
 import authReducer from './auth/reducer';
 
 export const store = configureStore({
   reducer: {
-    spotify: spotifyReducer,
     festivals: festivalsReducer,
     auth: authReducer
   },
