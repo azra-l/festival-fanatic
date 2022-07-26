9742a2c7bdae17130bf62e142ebe3ab284b1f483
Amman Zaman <amman.z.zaman@gmail.com>
2022-07-26 07:35:43 +0000

    updated schemas (refactored use of map for spotify imports to a separate collection)

--- server/models/Artist.js
+++ server/models/Artist.js
@@ -4,8 +4,7 @@ const Artist = new mongoose.Schema({
   name: String,
   spotify_id: String,
   bandintown_id: String,
-  external_urls: String,
-  id: String,
+  href: String,
 });
 
 module.exports = mongoose.model("Artist", Artist);
--- server/models/User.js
+++ server/models/User.js
@@ -1,41 +1,10 @@
 const mongoose = require("mongoose");
 
-const SpotifyArtistFollowersSchema = new mongoose.Schema({
-  href: String,
-  total: Number
-});
-
-const SpotifyArtistImageSchema = new mongoose.Schema({
-  height: Number,
-  width: Number,
-  url: String
-});
-
-const SpotifyArtistSchema = new mongoose.Schema({
-  followers: {
-    type: SpotifyArtistFollowersSchema
-  },
-  genres: [String],
-  href: String,
-  id: String,
-  images: [{
-    type: SpotifyArtistImageSchema
-  }],
-  name: String,
-  popularity: Number,
-  type: String,
-  uri: String
-});
-
 const User = new mongoose.Schema({
   userId: String,
   saved: [String],
   archived: [String],
-  spotifyImports: {
-    type: Map,
-    of: [SpotifyArtistSchema],
-    default: {}
-  }
+  selectedArtists: [{ type: mongoose.Schema.Types.ObjectId, ref: "Artist" }]
 });
 
 module.exports = mongoose.model("User", User);
