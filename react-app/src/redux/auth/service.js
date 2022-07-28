13d1124265e9dc4f62f3a810b9d7370b65b514d2
Amman Zaman <amman.z.zaman@gmail.com>
2022-07-28 13:20:45 +0000

    app and api and client urls are all specified in env now removed unused import warnings from fe

--- react-app/src/redux/auth/service.js
+++ react-app/src/redux/auth/service.js
@@ -1,11 +1,10 @@
+import { apiBaseUrl } from '../../utilities/base-url';
+
 const checkAuth = async () => {
-    const response = await fetch(`http://localhost:3001/users/check-auth`, {
+    const response = await fetch(`${apiBaseUrl}/users/check-auth`, {
         method: "GET",
         // Need credentials to pass cookie data into request
-        credentials: "include",
-        headers: {
-            "Access-Control-Allow-Origin": "http://localhost:3000",
-        },
+        credentials: "include"
     });
 
     console.log("The checkAuth response.status", response.status)
