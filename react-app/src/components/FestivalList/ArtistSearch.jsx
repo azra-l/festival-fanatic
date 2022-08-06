0a5bd98aeb29010e1c905f00a4b75f6fd14d814d
Tristan Martinuson <tmartinuson@gmail.com>
2022-08-06 00:10:22 +0000

    added base files for artist search component

---
+++ react-app/src/components/FestivalList/ArtistSearch.jsx
@@ -0,0 +1,4 @@
+import { Grid, TextField, InputLabel } from "@mui/material";
+import { createTheme, ThemeProvider, } from "@mui/material/styles";
+import React, { useEffect, useState } from "react";
+import "./ArtistSearch.css";
