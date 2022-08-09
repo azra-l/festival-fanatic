268d3eaf13fcf5ea2e23bbc30441175bb5b8c980
Azra Lalji <azralalji1@gmail.com>
2022-08-09 15:43:16 +0000

    use redux for selected artists

--- react-app/src/redux/festivals/thunks.js
+++ react-app/src/redux/festivals/thunks.js
@@ -4,6 +4,9 @@ import FestivalsService from "./service";
 export const actionTypes = {
   GET_EVENTS: "festivals/getUpcomingArtistEvents",
   UPDATE_FESTIVAL: "festivals/updateFestival",
+  GET_SELECTED_ARTISTS: "festivals/getSelectedArtists",
+  ADD_SELECTED_ARTISTS: "festivals/addSelectedArtists",
+  DELETE_SELECTED_ARTISTS: "festivals/deleteSelectedArtists",
 };
 
 export const getUpcomingArtistEventsAsync = createAsyncThunk(
@@ -19,3 +22,24 @@ export const updateFestivalAsync = createAsyncThunk(
     return await FestivalsService.updateFestival({ _id, action });
   }
 );
+
+export const getSelectedArtistsAsync = createAsyncThunk(
+  actionTypes.GET_SELECTED_ARTISTS,
+  async () => {
+    return await FestivalsService.getSelectedArtists();
+  }
+);
+
+export const addSelectedArtistAsync = createAsyncThunk(
+  actionTypes.ADD_SELECTED_ARTISTS,
+  async (value) => {
+    return await FestivalsService.addSelectedArtist(value);
+  }
+);
+
+export const deleteSelectedArtistAsync = createAsyncThunk(
+  actionTypes.DELETE_SELECTED_ARTISTS,
+  async (id) => {
+    return await FestivalsService.deleteSelectedArtistAsync(id);
+  }
+)
